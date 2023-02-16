3608b65239910b24f4b138ca1f5ed42d44697cba
vladimir.kozlov <vkozlov@lanit.ru>
2023-02-16 11:33:13 +0300

    Updates

--- precache-manifest.96359cc2428703b12b3a9344cf23d064.js
+++ precache-manifest.96359cc2428703b12b3a9344cf23d064.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "9cf209d19eaa53ca150f",
+    "revision": "11b751fda5acf2cf2e30",
     "url": "/gym_tracker/static/css/main.1c0cc54e.chunk.css"
   },
   {
-    "revision": "9cf209d19eaa53ca150f",
-    "url": "/gym_tracker/static/js/main.9cf209d1.chunk.js"
+    "revision": "11b751fda5acf2cf2e30",
+    "url": "/gym_tracker/static/js/main.11b751fd.chunk.js"
   },
   {
     "revision": "09fa53b2d3aba36d64b7",
@@ -24,44 +24,40 @@ self.__precacheManifest = [
     "url": "/gym_tracker/static/js/runtime~main.9f8e8548.js"
   },
   {
-    "revision": "d04413353a32aed8a0db452373452870",
-    "url": "/gym_tracker/static/media/roboto-cyrillic-ext-300-normal.d0441335.woff2"
+    "revision": "c2be5367fbf0e1066261fd78eb103f4a",
+    "url": "/gym_tracker/static/media/roboto-greek-ext-300-normal.c2be5367.woff2"
   },
   {
     "revision": "9b9ec29522d1bf8924ccc2d917e1807b",
     "url": "/gym_tracker/static/media/roboto-cyrillic-300-normal.9b9ec295.woff2"
   },
   {
-    "revision": "dcdaee374d5bbeab0a5ed5c8cf39a6cd",
-    "url": "/gym_tracker/static/media/roboto-greek-300-normal.dcdaee37.woff2"
-  },
-  {
-    "revision": "c2be5367fbf0e1066261fd78eb103f4a",
-    "url": "/gym_tracker/static/media/roboto-greek-ext-300-normal.c2be5367.woff2"
-  },
-  {
     "revision": "48c684d99330969e3ce90b9e9da2d698",
     "url": "/gym_tracker/static/media/roboto-vietnamese-300-normal.48c684d9.woff2"
   },
   {
-    "revision": "2323284ef85bfeaca074c668b72109c6",
-    "url": "/gym_tracker/static/media/roboto-all-300-normal.2323284e.woff"
+    "revision": "d04413353a32aed8a0db452373452870",
+    "url": "/gym_tracker/static/media/roboto-cyrillic-ext-300-normal.d0441335.woff2"
   },
   {
     "revision": "716871ec15f054ec158445180fe280e1",
     "url": "/gym_tracker/static/media/roboto-latin-ext-300-normal.716871ec.woff2"
   },
   {
+    "revision": "dcdaee374d5bbeab0a5ed5c8cf39a6cd",
+    "url": "/gym_tracker/static/media/roboto-greek-300-normal.dcdaee37.woff2"
+  },
+  {
     "revision": "b9c29351c46f3e8c8631c4002457f48a",
     "url": "/gym_tracker/static/media/roboto-latin-300-normal.b9c29351.woff2"
   },
   {
-    "revision": "c00467dc3792a8ab586955a3faefcac9",
-    "url": "/gym_tracker/static/media/roboto-cyrillic-ext-400-normal.c00467dc.woff2"
+    "revision": "2323284ef85bfeaca074c668b72109c6",
+    "url": "/gym_tracker/static/media/roboto-all-300-normal.2323284e.woff"
   },
   {
-    "revision": "d9ac47c7e500fb7083b8d595eaf6fe12",
-    "url": "/gym_tracker/static/media/roboto-cyrillic-400-normal.d9ac47c7.woff2"
+    "revision": "c00467dc3792a8ab586955a3faefcac9",
+    "url": "/gym_tracker/static/media/roboto-cyrillic-ext-400-normal.c00467dc.woff2"
   },
   {
     "revision": "35de3738b76d249ed060dd3d0f9286be",
@@ -72,8 +68,16 @@ self.__precacheManifest = [
     "url": "/gym_tracker/static/media/roboto-greek-400-normal.28668857.woff2"
   },
   {
+    "revision": "d9ac47c7e500fb7083b8d595eaf6fe12",
+    "url": "/gym_tracker/static/media/roboto-cyrillic-400-normal.d9ac47c7.woff2"
+  },
+  {
     "revision": "ca3b09b62fda648a4511700413313fd0",
     "url": "/gym_tracker/static/media/roboto-vietnamese-400-normal.ca3b09b6.woff2"
+  },
+  {
+    "revision": "62b936e168110e58e89e70ec82e22755",
+    "url": "/gym_tracker/static/media/roboto-all-400-normal.62b936e1.woff"
   },
   {
     "revision": "87ace20058325aa069320aa4af875dff",
@@ -84,32 +88,28 @@ self.__precacheManifest = [
     "url": "/gym_tracker/static/media/roboto-latin-400-normal.15d9f621.woff2"
   },
   {
-    "revision": "62b936e168110e58e89e70ec82e22755",
-    "url": "/gym_tracker/static/media/roboto-all-400-normal.62b936e1.woff"
-  },
-  {
     "revision": "2742d81afb69e902e4513dc7cdda0a7f",
     "url": "/gym_tracker/static/media/roboto-cyrillic-ext-500-normal.2742d81a.woff2"
-  },
-  {
-    "revision": "7b08b9e11fc6b8a8a1398b357e874144",
-    "url": "/gym_tracker/static/media/roboto-cyrillic-500-normal.7b08b9e1.woff2"
   },
   {
     "revision": "e7b7001dff6c14165abdc0fefdecae06",
     "url": "/gym_tracker/static/media/roboto-greek-ext-500-normal.e7b7001d.woff2"
   },
   {
+    "revision": "7b08b9e11fc6b8a8a1398b357e874144",
+    "url": "/gym_tracker/static/media/roboto-cyrillic-500-normal.7b08b9e1.woff2"
+  },
+  {
     "revision": "53f395eb854a40e978706b1082570e42",
     "url": "/gym_tracker/static/media/roboto-greek-500-normal.53f395eb.woff2"
   },
   {
-    "revision": "68d75d959b2a0e9958b11d781338c8f7",
-    "url": "/gym_tracker/static/media/roboto-all-500-normal.68d75d95.woff"
-  },
-  {
     "revision": "7cda2cfee99d697daf8c14819d9004eb",
     "url": "/gym_tracker/static/media/roboto-vietnamese-500-normal.7cda2cfe.woff2"
+  },
+  {
+    "revision": "68d75d959b2a0e9958b11d781338c8f7",
+    "url": "/gym_tracker/static/media/roboto-all-500-normal.68d75d95.woff"
   },
   {
     "revision": "e36fccd06262bef92e7a9841e2202225",
@@ -124,16 +124,12 @@ self.__precacheManifest = [
     "url": "/gym_tracker/static/media/roboto-cyrillic-ext-700-normal.e0bc9313.woff2"
   },
   {
-    "revision": "6f112ec2b932ee12379442c42853244e",
-    "url": "/gym_tracker/static/media/roboto-cyrillic-700-normal.6f112ec2.woff2"
-  },
-  {
     "revision": "2953af0021626d3c3078b17590118908",
     "url": "/gym_tracker/static/media/roboto-greek-ext-700-normal.2953af00.woff2"
   },
   {
-    "revision": "aa462125b8faf7600001e1fe9b47e216",
-    "url": "/gym_tracker/static/media/roboto-all-700-normal.aa462125.woff"
+    "revision": "6f112ec2b932ee12379442c42853244e",
+    "url": "/gym_tracker/static/media/roboto-cyrillic-700-normal.6f112ec2.woff2"
   },
   {
     "revision": "3f8b2aa43c439ca2c8930c198320c231",
@@ -144,6 +140,10 @@ self.__precacheManifest = [
     "url": "/gym_tracker/static/media/roboto-vietnamese-700-normal.cdaab836.woff2"
   },
   {
+    "revision": "aa462125b8faf7600001e1fe9b47e216",
+    "url": "/gym_tracker/static/media/roboto-all-700-normal.aa462125.woff"
+  },
+  {
     "revision": "deb26e9b1a25438118e5d39d741ae6b6",
     "url": "/gym_tracker/static/media/roboto-latin-ext-700-normal.deb26e9b.woff2"
   },
@@ -152,7 +152,7 @@ self.__precacheManifest = [
     "url": "/gym_tracker/static/media/roboto-latin-700-normal.e9f5aaf5.woff2"
   },
   {
-    "revision": "3002376b776fcd6da2278ee9b5d48b89",
+    "revision": "6af04405e710b98bdbec5be56f2c89ea",
     "url": "/gym_tracker/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gym_tracker/precache-manifest.6c1f53baffe39d4563d59d7d82d7fd83.js"
+  "/gym_tracker/precache-manifest.96359cc2428703b12b3a9344cf23d064.js"
 );
 
 workbox.clientsClaim();
