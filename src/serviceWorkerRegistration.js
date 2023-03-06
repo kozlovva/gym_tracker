8eb66956b25e1d642884d13285142dbc55d6be04
vladimir.kozlov <vkozlov@lanit.ru>
2023-03-06 20:27:47 +0300

    test

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -18,17 +18,17 @@ const isLocalhost = Boolean(
     window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
 );
 
-function forceSWupdate () {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.getRegistrations().then(function (registrations) {
-      for (let registration of registrations) {
-        registration.update()
-      }
-    })
-  }
-}
+// function forceSWupdate () {
+//   if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.getRegistrations().then(function (registrations) {
+//       for (let registration of registrations) {
+//         registration.update()
+//       }
+//     })
+//   }
+// }
 
-forceSWupdate()
+// forceSWupdate()
 
 export function register(config) {
   if ('serviceWorker' in navigator) {
