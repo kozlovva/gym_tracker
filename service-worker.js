258351f0d7df5ca067d65d077ec2e48b407263d2
vladimir.kozlov <vkozlov@lanit.ru>
2023-03-13 18:48:25 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gym_tracker/precache-manifest.0b3aa8268beabd8542158b97c1dc2042.js"
+  "/gym_tracker/precache-manifest.c4ce1a80d4932096e4d74bf9fd04ab67.js"
 );
 
 workbox.clientsClaim();
